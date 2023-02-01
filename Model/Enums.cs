f7f1c63e1732ad6efac5f51d821490f79326c163
Tom <tom.a.antoine@free.fr>
2023-02-01 14:02:27 +0100

    Project Initial Commit

---
+++ Model/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Model.Enums
+{
+        public enum SaveType
+        {
+            Complete,
+            Differential
+        }
+}
---
+++ Model/SaveModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySave.Model.Enums;
+
+namespace EasySave.Model
+{
+    public class SaveModel
+    {
+        public string Name { get; set; }
+        public string SourceRepo { get; set; }
+        public string DestinationRepo { get; set; } 
+        public SaveType Type { get; set; }
+    }
+}
