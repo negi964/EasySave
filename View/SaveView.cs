fbab6256491f07af9ba289068d8748722d6296d5
Paul-Louagie <louagie27@gmail.com>
2023-02-02 10:52:07 +0100

    add menu

--- View/SaveView.cs
+++ View/SaveView.cs
@@ -12,9 +12,24 @@ namespace EasySave.View
 {
     public class SaveView
     {
+        private string numOption;
         public SaveView()
         {
             new SaveViewModel(this);
+
+            Console.WriteLine(" _______     ___           _______.____    ____  _______.     ___   ____    ____  _______ \r\n|   ____|   /   \\         /       |\\   \\  /   / /       |    /   \\  \\   \\  /   / |   ____|\r\n|  |__     /  ^  \\       |   (----` \\   \\/   / |   (----`   /  ^  \\  \\   \\/   /  |  |__   \r\n|   __|   /  /_\\  \\       \\   \\      \\_    _/   \\   \\      /  /_\\  \\  \\      /   |   __|  \r\n|  |____ /  _____  \\  .----)   |       |  | .----)   |    /  _____  \\  \\    /    |  |____ \r\n|_______/__/     \\__\\ |_______/        |__| |_______/    /__/     \\__\\  \\__/     |_______|\r\n                                                                                          ");
+            Console.WriteLine("");
+            Console.WriteLine("Bienvenue !");
+            Console.WriteLine("");
+            Console.WriteLine("Choisissez un chiffre entre 1 et 4 : ");
+            Console.WriteLine("");
+            Console.WriteLine("[1]. Changer de langue");
+            Console.WriteLine("[2]. Créer une sauvegarde");
+            Console.WriteLine("[3]. Modifier une sauvegarde");
+            Console.WriteLine("[4]. Supprimer une sauvegarde");
+            Console.WriteLine("");
+            Console.Write("Entrer un chiffre : ");
+            numOption = Console.ReadLine();
         }
     }
 }
