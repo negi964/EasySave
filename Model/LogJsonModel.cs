8bfd1d2f61dcabc61d03e63d66128feb1b4ed8c4
Lorenzo <lorenzo.desisto@viacesi.fr>
2023-02-07 11:03:07 +0100

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Model/LogJsonModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//NewtonsoftJson
+using NewtonsoftJson = Newtonsoft.Json;
+using EasySave.Model;
+//serialisation
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+using static EasySave.Model.SaveModel;
+using Newtonsoft.Json;
+using System.Security.Principal;
+
+namespace EasySave.Model
+{
+    public class LogJsonModel
+    {
+        public string Name { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string FileSource { get; set; }
+        public string FileTarget { get; set; }
+        public float FileSize  { get; set; }
+        public int TransfertTime { get; set;}
+
+        public BackupConfig backupconfig { get; set; }
+
+        public LogJsonModel() { }
+
+        public LogJsonModel(string name, string fileSource, string fileTarget, float fileSize, int transfertTime)
+        {
+            name = backupconfig.Name;
+             TimeStamp = DateTime.Now;
+            FileSource = backupconfig.SourceDirectory;
+            FileTarget = backupconfig.TargetDirectory;
+            FileSize = fileSize;
+            TransfertTime = transfertTime;
+        }
+
+        public BackupConfig ReadJsonConfig(string path)
+        {
+            string fileContent = File.ReadAllText(path);
+
+            BackupConfig JsonConfig = JsonConvert.DeserializeObject<BackupConfig>(fileContent);
+            return JsonConfig;
+        }
+        
+        
+
+        public void savelog(BackupConfig config)
+        {
+            
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(FileTarget))
+                {
+                    string logjson = ("Name : " +Name + "\n FileSource : " +FileSource + "\n FileTarget : " + FileTarget + "\n FileSize :"+FileSize + "\n TransfertTime :"+TransfertTime); 
+                     writer.WriteLine(logjson);
+                }
+            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nUne erreur s'est produite lors de l'enregistrement du log journalier: " + ex.Message);
+            }
+
+        }
+    }
+
+}
