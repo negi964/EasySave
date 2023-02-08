65e5d831e50ace99ff5bb32fbea6ea69513de335
Lorenzo <lorenzo.desisto@viacesi.fr>
2023-02-08 16:37:13 +0100

    ADD LogState

---
+++ Model/StatelogModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Model
+{
+    public class Statelog : LogJsonModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+
+        public Statelog() : base() { }
+       
+   
+
+
+    }
+   
+}
