7edc2202432ce7eab9a8ab525cc9f2f66e6b09c2
Paul-Louagie <louagie27@gmail.com>
2023-02-08 12:08:47 +0100

    Add delete

--- View-Model/EditViewModel.cs
+++ View-Model/EditViewModel.cs
@@ -10,32 +10,31 @@ namespace EasySave.View_Model
 {
     public class EditViewModel
     {
-        private EditModel editModel;
+        private EditModel editModel = new EditModel();
         public EditViewModel()
         {
-            var editModel = new EditModel();
         }
 
         public void GetEditModel(string newBackupName, string newSourceDirectory, string newTargetDirectory, string newBackupType, int configToModify)
         {
             //var editModel = new EditModel();
-            var message = editModel.EditSave(newBackupName, newSourceDirectory, newTargetDirectory, newBackupType, configToModify);
+            /*var message = editModel.EditSave(newBackupName, newSourceDirectory, newTargetDirectory, newBackupType, configToModify);
             if (message != null)
             {
                 var error = new ErrorView();
                 error.ShowError(message);
                 GetEditModel(newBackupName, newSourceDirectory, newTargetDirectory, newBackupType, configToModify);
-            }
+            }*/
         }
 
         public void GetListConfig()
         {
-            var message = editModel.GetListConfigToModify();
+            /*var message = editModel.GetListConfigToModify();
             if (message != null)
             {
                 var error = new ErrorView();
                 error.ShowError(message);
-            }
+            }*/
         }
     }
 }
